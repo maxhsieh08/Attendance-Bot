e16df0ea02aac85a7d5d517ce773888d7d82f562
Max Hsieh <maxhsieh0@gmail.com>
2023-11-19 23:10:02 -0800

    added functionality to streak

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,28 @@
 const { Events } = require("discord.js");
 const schedule = require("node-schedule");
+const fs = require("fs");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    const channel = client.channels.cache.get("1175256511468011630");
+
     schedule.scheduleJob("alert", "0 6 * * *", () => {
-      const channel = client.channels.cache.get("1175256511468011630");
-      channel.send("<@&1175178345466581192> project time");
+      channel.send("project time");
+      const rawData = fs.readFileSync("userData.json");
+      const users = JSON.parse(rawData);
+      const currentDay = new Date();
+      for (const streak of users.users) {
+        const date = new Date(streak.lastClockIn).getDate();
+        if (date !== currentDay.getDate() - 1) {
+          streak.checkInStreak = 0;
+        }
+      }
+      fs.writeFileSync("userData.json", JSON.stringify(users, null, 2));
     });
     schedule.scheduleJob("alert", "0 8 * * *", () => {
-      const channel = client.channels.cache.get("1175256511468011630");
       channel.send("<@&1175178345466581192> one more hour to check in");
     });
   },
