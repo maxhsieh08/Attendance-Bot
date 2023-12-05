366b38ef465af96a3d1bc88dfc30eec35e9c33e0
Max Hsieh <maxhsieh0@gmail.com>
2023-12-04 23:26:01 -0800

    added time zone to job

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,12 @@ module.exports = {
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
     const channel = client.channels.cache.get("1175256511468011630");
-    schedule.scheduleJob("alert", "0 6 * * *", () => {
+    const rule = new schedule.RecurrenceRule();
+    rule.tz = "America/Los_Angeles";
+    rule.second = 0;
+    rule.minute = 0;
+    rule.hour = 6;
+    schedule.scheduleJob(rule, () => {
       channel.send("<@&1175178345466581192> project time");
       const rawData = fs.readFileSync("userData.json");
       const users = JSON.parse(rawData);
@@ -23,8 +28,13 @@ module.exports = {
       }
       fs.writeFileSync("userData.json", JSON.stringify(users, null, 2));
     });
-    schedule.scheduleJob("alert", "0 8 * * *", () => {
-      channel.send("<@&1175178345466581192> one more hour to check in");
+    const pingEnd = new schedule.RecurrenceRule();
+    pingEnd.tz = "America/Los_Angeles";
+    pingEnd.second = 0;
+    pingEnd.minute = 0;
+    pingEnd.hour = 9;
+    schedule.scheduleJob(pingEnd, () => {
+      channel.send("<@&1175178345466581192> check-ins closed for todasy");
     });
   },
 };
--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const botToken = process.env.BOT_TOKEN;
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app");
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
